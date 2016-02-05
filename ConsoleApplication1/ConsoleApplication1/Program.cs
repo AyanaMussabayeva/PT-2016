d6c33d8556c8e7ffb2e9527f43ea78abaf3b5792
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2016-02-05 18:22:28 +0600

    Oops, that's the wrong repository

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using Snake.model;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
 
 namespace Snake
 {
@@ -10,6 +13,41 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Game.Init();
+            Game.LoadlLevel(1);
+
+            while (Game.isActive)
+            {
+                Game.Draw();
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Game.snake.Move(0, -1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Game.snake.Move(0, 1);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Game.snake.Move(-1, 0);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Game.snake.Move(1, 0);
+                        break;
+                    case ConsoleKey.Escape:
+                        Game.isActive = false;
+                        break;
+                    case ConsoleKey.F2:
+                        Game.Save();
+                        break;
+                    case ConsoleKey.F3:
+                        Game.Resume();
+                        break;
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
