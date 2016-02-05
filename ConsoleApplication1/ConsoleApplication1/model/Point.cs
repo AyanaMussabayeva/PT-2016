1a53d350e98cf68ddedb1ac3339b7012fb0f97c6
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2016-02-05 11:14:41 +0600

    https://github.com/AyanaMussabayeva/Snake-.git

---
+++ ConsoleApplication1/ConsoleApplication1/model/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.model
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public Point()
+        {
+
+        }
+    }
+}
