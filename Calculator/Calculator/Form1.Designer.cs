09a3203bedd3a697e8203b8b7c21f9719691e9cb
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2016-02-20 11:48:36 +0600

    calc beta-version

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pad1 = new System.Windows.Forms.Button();
+            this.pad2 = new System.Windows.Forms.Button();
+            this.pad3 = new System.Windows.Forms.Button();
+            this.pad6 = new System.Windows.Forms.Button();
+            this.pad5 = new System.Windows.Forms.Button();
+            this.pad4 = new System.Windows.Forms.Button();
+            this.pad9 = new System.Windows.Forms.Button();
+            this.pad8 = new System.Windows.Forms.Button();
+            this.pad7 = new System.Windows.Forms.Button();
+            this.pad0 = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.TextBox();
+            this.operationProduct = new System.Windows.Forms.Button();
+            this.operationMinus = new System.Windows.Forms.Button();
+            this.operationPlus = new System.Windows.Forms.Button();
+            this.operationDevision = new System.Windows.Forms.Button();
+            this.padDouble = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Button();
+            this.modificationInverse = new System.Windows.Forms.Button();
+            this.modificationSqrt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pad1
+            // 
+            this.pad1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pad1.Location = new System.Drawing.Point(183, 160);
+            this.pad1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad1.Name = "pad1";
+            this.pad1.Size = new System.Drawing.Size(73, 66);
+            this.pad1.TabIndex = 0;
+            this.pad1.Text = "1";
+            this.pad1.UseVisualStyleBackColor = true;
+            this.pad1.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad2
+            // 
+            this.pad2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pad2.Location = new System.Drawing.Point(264, 160);
+            this.pad2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad2.Name = "pad2";
+            this.pad2.Size = new System.Drawing.Size(73, 66);
+            this.pad2.TabIndex = 1;
+            this.pad2.Text = "2";
+            this.pad2.UseVisualStyleBackColor = true;
+            this.pad2.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad3
+            // 
+            this.pad3.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pad3.Location = new System.Drawing.Point(344, 160);
+            this.pad3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad3.Name = "pad3";
+            this.pad3.Size = new System.Drawing.Size(73, 66);
+            this.pad3.TabIndex = 2;
+            this.pad3.Text = "3";
+            this.pad3.UseVisualStyleBackColor = true;
+            this.pad3.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad6
+            // 
+            this.pad6.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pad6.Location = new System.Drawing.Point(344, 234);
+            this.pad6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad6.Name = "pad6";
+            this.pad6.Size = new System.Drawing.Size(73, 66);
+            this.pad6.TabIndex = 5;
+            this.pad6.Text = "6";
+            this.pad6.UseVisualStyleBackColor = true;
+            this.pad6.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad5
+            // 
+            this.pad5.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pad5.Location = new System.Drawing.Point(264, 234);
+            this.pad5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad5.Name = "pad5";
+            this.pad5.Size = new System.Drawing.Size(73, 66);
+            this.pad5.TabIndex = 4;
+            this.pad5.Text = "5";
+            this.pad5.UseVisualStyleBackColor = true;
+            this.pad5.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad4
+            // 
+            this.pad4.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pad4.Location = new System.Drawing.Point(183, 234);
+            this.pad4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad4.Name = "pad4";
+            this.pad4.Size = new System.Drawing.Size(73, 66);
+            this.pad4.TabIndex = 3;
+            this.pad4.Text = "4";
+            this.pad4.UseVisualStyleBackColor = true;
+            this.pad4.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad9
+            // 
+            this.pad9.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pad9.Location = new System.Drawing.Point(344, 308);
+            this.pad9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad9.Name = "pad9";
+            this.pad9.Size = new System.Drawing.Size(73, 66);
+            this.pad9.TabIndex = 8;
+            this.pad9.Text = "9";
+            this.pad9.UseVisualStyleBackColor = true;
+            this.pad9.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad8
+            // 
+            this.pad8.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pad8.Location = new System.Drawing.Point(264, 308);
+            this.pad8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad8.Name = "pad8";
+            this.pad8.Size = new System.Drawing.Size(73, 66);
+            this.pad8.TabIndex = 7;
+            this.pad8.Text = "8";
+            this.pad8.UseVisualStyleBackColor = true;
+            this.pad8.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad7
+            // 
+            this.pad7.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pad7.Location = new System.Drawing.Point(183, 308);
+            this.pad7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad7.Name = "pad7";
+            this.pad7.Size = new System.Drawing.Size(73, 66);
+            this.pad7.TabIndex = 6;
+            this.pad7.Text = "7";
+            this.pad7.UseVisualStyleBackColor = true;
+            this.pad7.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // pad0
+            // 
+            this.pad0.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pad0.Location = new System.Drawing.Point(183, 382);
+            this.pad0.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pad0.Name = "pad0";
+            this.pad0.Size = new System.Drawing.Size(73, 66);
+            this.pad0.TabIndex = 9;
+            this.pad0.Text = "0";
+            this.pad0.UseVisualStyleBackColor = true;
+            this.pad0.Click += new System.EventHandler(this.pad_Click);
+            // 
+            // display
+            // 
+            this.display.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.display.Location = new System.Drawing.Point(183, 92);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(347, 37);
+            this.display.TabIndex = 10;
+            // 
+            // operationProduct
+            // 
+            this.operationProduct.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.operationProduct.Location = new System.Drawing.Point(457, 308);
+            this.operationProduct.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.operationProduct.Name = "operationProduct";
+            this.operationProduct.Size = new System.Drawing.Size(73, 66);
+            this.operationProduct.TabIndex = 13;
+            this.operationProduct.Text = "*";
+            this.operationProduct.UseVisualStyleBackColor = true;
+            this.operationProduct.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // operationMinus
+            // 
+            this.operationMinus.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operationMinus.Location = new System.Drawing.Point(457, 234);
+            this.operationMinus.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.operationMinus.Name = "operationMinus";
+            this.operationMinus.Size = new System.Drawing.Size(73, 66);
+            this.operationMinus.TabIndex = 12;
+            this.operationMinus.Text = "-";
+            this.operationMinus.UseVisualStyleBackColor = true;
+            this.operationMinus.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // operationPlus
+            // 
+            this.operationPlus.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.operationPlus.Location = new System.Drawing.Point(457, 160);
+            this.operationPlus.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.operationPlus.Name = "operationPlus";
+            this.operationPlus.Size = new System.Drawing.Size(73, 66);
+            this.operationPlus.TabIndex = 11;
+            this.operationPlus.Text = "+";
+            this.operationPlus.UseVisualStyleBackColor = true;
+            this.operationPlus.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // operationDevision
+            // 
+            this.operationDevision.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.operationDevision.Location = new System.Drawing.Point(457, 382);
+            this.operationDevision.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.operationDevision.Name = "operationDevision";
+            this.operationDevision.Size = new System.Drawing.Size(73, 66);
+            this.operationDevision.TabIndex = 14;
+            this.operationDevision.Text = "/";
+            this.operationDevision.UseVisualStyleBackColor = true;
+            this.operationDevision.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // padDouble
+            // 
+            this.padDouble.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.padDouble.Location = new System.Drawing.Point(344, 382);
+            this.padDouble.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.padDouble.Name = "padDouble";
+            this.padDouble.Size = new System.Drawing.Size(73, 66);
+            this.padDouble.TabIndex = 15;
+            this.padDouble.Text = ".";
+            this.padDouble.UseVisualStyleBackColor = true;
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(536, 382);
+            this.result.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(73, 66);
+            this.result.TabIndex = 19;
+            this.result.Text = "=";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // modificationInverse
+            // 
+            this.modificationInverse.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.modificationInverse.Location = new System.Drawing.Point(536, 308);
+            this.modificationInverse.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.modificationInverse.Name = "modificationInverse";
+            this.modificationInverse.Size = new System.Drawing.Size(73, 66);
+            this.modificationInverse.TabIndex = 18;
+            this.modificationInverse.Text = "1/x";
+            this.modificationInverse.UseVisualStyleBackColor = true;
+            // 
+            // modificationSqrt
+            // 
+            this.modificationSqrt.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modificationSqrt.Location = new System.Drawing.Point(536, 234);
+            this.modificationSqrt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.modificationSqrt.Name = "modificationSqrt";
+            this.modificationSqrt.Size = new System.Drawing.Size(73, 66);
+            this.modificationSqrt.TabIndex = 17;
+            this.modificationSqrt.Text = "sqrt";
+            this.modificationSqrt.UseVisualStyleBackColor = true;
+            this.modificationSqrt.Click += new System.EventHandler(this.operation_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 561);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.modificationInverse);
+            this.Controls.Add(this.modificationSqrt);
+            this.Controls.Add(this.padDouble);
+            this.Controls.Add(this.operationDevision);
+            this.Controls.Add(this.operationProduct);
+            this.Controls.Add(this.operationMinus);
+            this.Controls.Add(this.operationPlus);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.pad0);
+            this.Controls.Add(this.pad9);
+            this.Controls.Add(this.pad8);
+            this.Controls.Add(this.pad7);
+            this.Controls.Add(this.pad6);
+            this.Controls.Add(this.pad5);
+            this.Controls.Add(this.pad4);
+            this.Controls.Add(this.pad3);
+            this.Controls.Add(this.pad2);
+            this.Controls.Add(this.pad1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pad1;
+        private System.Windows.Forms.Button pad2;
+        private System.Windows.Forms.Button pad3;
+        private System.Windows.Forms.Button pad6;
+        private System.Windows.Forms.Button pad5;
+        private System.Windows.Forms.Button pad4;
+        private System.Windows.Forms.Button pad9;
+        private System.Windows.Forms.Button pad8;
+        private System.Windows.Forms.Button pad7;
+        private System.Windows.Forms.Button pad0;
+        private System.Windows.Forms.TextBox display;
+        private System.Windows.Forms.Button operationProduct;
+        private System.Windows.Forms.Button operationMinus;
+        private System.Windows.Forms.Button operationPlus;
+        private System.Windows.Forms.Button operationDevision;
+        private System.Windows.Forms.Button padDouble;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Button modificationInverse;
+        private System.Windows.Forms.Button modificationSqrt;
+    }
+}
+
---
+++ Calculator/Calculator/Model/BaseCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Model
+{
+        public class BaseCalculator
+        {
+            public double firstNumber = 0;
+            public double secondNumber = 0;
+            public double resultNumber = 0;
+            public State currentState = State.WaitingForFirstNumber;
+            public Operation operation = Operation.None;
+
+            public string Evaluate(string text)
+            {
+                secondNumber = double.Parse(text);
+                currentState = State.WaitingForFirstNumber;
+
+                switch (operation)
+                {
+                    case Operation.None:
+                        break;
+                    case Operation.Plus:
+                        resultNumber = firstNumber + secondNumber;
+                        break;
+                    case Operation.Minus:
+                        resultNumber = firstNumber - secondNumber;
+                        break;
+                    case Operation.Divide:
+                        resultNumber = firstNumber / secondNumber;
+                        break;
+                    case Operation.Product:
+                        resultNumber = firstNumber * secondNumber;
+                        break;
+
+                    case Operation.Sqrt:
+                        resultNumber =  Math.Sqrt(secondNumber);
+                    break;
+                default:
+                        break;
+                
+            }
+
+                return resultNumber.ToString();
+            }
+        }
+    
+}
