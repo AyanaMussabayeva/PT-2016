1c1ae87b40a5b8be495c00e7724a66a954201533
AyanaMussabayeva <ayana.mussabayeva@gmail.com>
2016-03-05 22:25:37 +0600

    timer label added

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.IO;
 using Snake.model;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
-
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -17,10 +14,35 @@ namespace Snake
     /// </summary>
     class Program
     {
+        
         public static int level = 1;
+        public static Timer time = new Timer(new TimerCallback(seconding), st, 0, 1000);
+       
+
+        public static int seconds = 0;
+        public static int minuts = 0;
+        private static int st;
+
+        public static void seconding(object st)
+        {
+            seconds++;
+            if (seconds == 60)
+            {
+                minuts++;
+                seconds = 0;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(3, 47);
+            Console.WriteLine("Time " + minuts + " : " + seconds);
+        }
+
+
+        public static string dir = "Right";
+       
         static void Main(string[] args)
         {
-            while(level<= Directory.GetFiles(@"C:\Users\Admin\Source\Repos\NewRepo\ConsoleApplication1\ConsoleApplication1\bin\Debug\Levels").Length)
+            
+            while (level <= Directory.GetFiles(@"C:\Users\Admin\Source\Repos\NewRepo\ConsoleApplication1\ConsoleApplication1\bin\Debug\Levels").Length)
             {
                 Game.Init();
                 Game.LoadlLevel(level);
@@ -34,16 +56,20 @@ namespace Snake
                     switch (pressedKey.Key)
                     {
                         case ConsoleKey.UpArrow:
-                            Game.snake.Move(0, -1);
+                            //Game.snake.Move(0, -1);
+                            dir = "Up";
                             break;
                         case ConsoleKey.DownArrow:
-                            Game.snake.Move(0, 1);
+                            //Game.snake.Move(0, 1);
+                            dir = "Down";
                             break;
                         case ConsoleKey.LeftArrow:
-                            Game.snake.Move(-1, 0);
+                            //Game.snake.Move(-1, 0);
+                            dir = "Left";
                             break;
                         case ConsoleKey.RightArrow:
-                            Game.snake.Move(1, 0);
+                            //Game.snake.Move(1, 0);
+                            dir = "Right";
                             break;
                         case ConsoleKey.Escape:
                             Game.isActive = false;
@@ -55,13 +81,33 @@ namespace Snake
                             Game.Resume();
                             break;
                     }
-                }
+                    switch (dir)// меняем положение 
+                    {
+                        case "Up":
+                            Game.snake.Move(0, -1);
+                            break;
+                        case "Down":
+                            Game.snake.Move(0, 1);
+                            break;
+                        case "Left":
+                            Game.snake.Move(-1, 0);
+                            break;
+                        case "Right":
+                            Game.snake.Move(1, 0);
+                            break;
 
+                    }
+                }
+                
+
+                //Game.wall.Draw();// убираем мерцание стены 
                 Console.ReadKey();
             }
             Console.Clear();
             Console.WriteLine("You won!");
         }
-        
+
     }
 }
+
+
